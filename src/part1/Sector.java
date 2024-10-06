34f0ce74e3ed5add6db75bcf4aed529591530c92
AsumVictor <iamasum369@gmail.com>
2024-10-06 01:50:04 +0000

    Added template code

---
+++ src/part1/Sector.java
@@ -0,0 +1,21 @@
+package part1;
+
+import java.util.Map;
+
+public class Sector {
+    private String name;
+    private Map<Integer, Double> emissions;
+
+    public Sector(String name, Map<Integer, Double> emissions) {
+        this.name = name;
+        this.emissions = emissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<Integer, Double> getEmissions() {
+        return emissions;
+    }
+}
---
+++ src/part2/Sector.java
@@ -0,0 +1,21 @@
+package part2;
+
+import java.util.Map;
+
+public class Sector {
+    private String name;
+    private Map<Integer, Double> emissions;
+
+    public Sector(String name, Map<Integer, Double> emissions) {
+        this.name = name;
+        this.emissions = emissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<Integer, Double> getEmissions() {
+        return emissions;
+    }
+}
